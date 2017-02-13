80048341d76cc3ab8b389460e9c4e37cdd0c3822
mathewabado <mathew.abado@gmail.com>
2017-02-13 10:06:32 -0500

    Added Course class and made changes to Student and Instructor.

---
+++ src/Course.java
@@ -0,0 +1,14 @@
+/**
+ * Created by mabado on 13/02/17.
+ */
+public class Course
+{
+    String cname;
+    String description;
+
+    public Course(String cname, String description)
+    {
+        this.cname = cname;
+        this.description = description;
+    }
+}
